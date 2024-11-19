25464ec09da6cbcadf78a8a710d1c92f60725c13
Rupendra Kumar Dhiman <Dhiman.RupendraK@hutchisonports.com>
2024-11-19 17:04:21 +0800

    Zoom increase from 10 to 7

--- src/containers/WardrobeContainer/WardrobeContainer.tsx
+++ src/containers/WardrobeContainer/WardrobeContainer.tsx
@@ -8,7 +8,7 @@ export const WardrobeContainer = () => {
   const { currentWardrobe, wardrobeInnerColor = '#CCC', showWireframe = false, showDoors = false } = useSelector((state: IApplicationState) => state.wardrobe);
 
   return <div className="main-container">
-    <Canvas camera={{ fov: 10, near: 0.1, far: 1000, position: [-30, 4, -4] }}>
+    <Canvas camera={{ fov: 7, near: 0.1, far: 1000, position: [-30, 4, -4] }}>
       <Wardrobe innerColor={wardrobeInnerColor} wardrobeColor={currentWardrobe.wardrobeColor} size={currentWardrobe.size} hangerRoadSize={{ ...currentWardrobe.size, height: 48 }} showWireFrame={showWireframe} showDoors={showDoors} />
     </Canvas>
 
